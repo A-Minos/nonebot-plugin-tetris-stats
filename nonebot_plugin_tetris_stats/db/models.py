37991c0174b317c9ea7bff695ca4475b56144aed
scdhh <wallfjjd@gmail.com>
2023-01-31 02:46:10 +0800

    🗃️ 调整数据库索引

--- nonebot_plugin_tetris_stats/db/models.py
+++ nonebot_plugin_tetris_stats/db/models.py
@@ -21,13 +21,13 @@ class Bind(Model):
 
 class Historical_Data(Model):
     id = IntField(pk=True, generated=True)
-    receive_time = DatetimeField(null=True)
+    receive_time = DatetimeField(null=True, index=True)
     bot_id = CharField(max_length=16, null=True)
     source_type = TextField(null=True)
     source_id = TextField(null=True)
-    message_id = IntField(null=True, index=True)
+    message_id = IntField(null=True)
     message = TextField(null=True)
-    call_time = DatetimeField(null=True)
+    call_time = DatetimeField(null=True, index=True)
     game_type = CharField(max_length=16, null=True)
     command_type = CharField(max_length=16, null=True)
     command_args = TextField(null=True)
@@ -35,7 +35,7 @@ class Historical_Data(Model):
     response = JSONField(null=True)
     processed_data = JSONField(null=True)
     return_message = BinaryField(null=True)
-    send_time = DatetimeField(null=True)
+    send_time = DatetimeField(null=True, index=True)
 
 
 class Version(Model):
