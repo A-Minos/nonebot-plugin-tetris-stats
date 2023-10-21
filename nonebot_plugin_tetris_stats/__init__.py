2a24cba2a5c1380e727f380bf010a722022330d3
scdhh <wallfjjd@gmail.com>
2023-10-21 22:39:18 +0800

    ✨ 添加插件元数据

--- nonebot_plugin_tetris_stats/__init__.py
+++ nonebot_plugin_tetris_stats/__init__.py
@@ -1,6 +1,22 @@
 from nonebot import require
+from nonebot.plugin import PluginMetadata
+
+from .config.config import migrations
 
 require('nonebot_plugin_localstore')
 require('nonebot_plugin_orm')
+
+
+__plugin_meta__ = PluginMetadata(
+    name='Tetris Stats',
+    description='一个用于查询 Tetris 相关游戏玩家数据的插件',
+    usage='发送 {游戏名}查 | query | stats{用户名/ID} 查询数据\n发送 {游戏名}绑定 | bind{用户名/ID} 绑定账号',
+    type='application',
+    homepage='https://github.com/shoucandanghehe/nonebot-plugin-tetris-stats',
+    supported_adapters={'~onebot.v11', '~qq'},
+    extra={
+        'orm_version_location': migrations,
+    },
+)
 
 from . import game_data_processor  # noqa: F401, E402
