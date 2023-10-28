b4032879201af3e682d4b6190f3e0e9450aba281
scdhh <wallfjjd@gmail.com>
2023-10-29 00:12:08 +0800

    🔥 删除一些不需要的常量

--- nonebot_plugin_tetris_stats/game_data_processor/top_data_processor/constant.py
+++ nonebot_plugin_tetris_stats/game_data_processor/top_data_processor/constant.py
@@ -1,6 +1,4 @@
 from ...utils.typing import GameType
 
 GAME_TYPE: GameType = 'TOP'
-BIND_COMMAND: list[str] = ['top绑定', 'topbind']
-QUERY_COMMAND: list[str] = ['top查', 'topquery', 'topstats']
 BASE_URL = 'http://tetrisonline.pl/top/'
--- nonebot_plugin_tetris_stats/game_data_processor/tos_data_processor/constant.py
+++ nonebot_plugin_tetris_stats/game_data_processor/tos_data_processor/constant.py
@@ -1,13 +1,4 @@
 from ...utils.typing import GameType
 
 GAME_TYPE: GameType = 'TOS'
-QUERY_COMMAND = [
-    'tos查',
-    'tostats',
-    'tosstats',
-    'tosquery',
-    '茶服查',
-    '茶服stats',
-    '茶服query',
-]
 BASE_URL = 'https://teatube.cn:8888/'
