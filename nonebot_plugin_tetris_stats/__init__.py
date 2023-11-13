61bcb22dfb1a1c8b7fb0bf6cd23bbe2a39482d00
scdhh <wallfjjd@gmail.com>
2023-11-13 20:03:54 +0800

    ✨ 更新 PluginMetadata

--- nonebot_plugin_tetris_stats/__init__.py
+++ nonebot_plugin_tetris_stats/__init__.py
@@ -11,10 +11,9 @@ from .config.config import migrations  # noqa: E402
 __plugin_meta__ = PluginMetadata(
     name='Tetris Stats',
     description='一个用于查询 Tetris 相关游戏玩家数据的插件',
-    usage='发送 {游戏名}查 | query | stats{用户名/ID} 查询数据\n发送 {游戏名}绑定 | bind{用户名/ID} 绑定账号',
+    usage='发送 {游戏名} --help 查询使用方法',
     type='application',
     homepage='https://github.com/shoucandanghehe/nonebot-plugin-tetris-stats',
-    supported_adapters={'~onebot.v11', '~qq'},
     extra={
         'orm_version_location': migrations,
     },
