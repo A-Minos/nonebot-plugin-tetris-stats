e5b23b0fe373577eba45b50489c5846150d8bf6d
scdhh <wallfjjd@gmail.com>
2023-12-30 04:03:22 +0800

    ✨ 适配 kook 茶服查target

--- nonebot_plugin_tetris_stats/game_data_processor/tos_data_processor/__init__.py
+++ nonebot_plugin_tetris_stats/game_data_processor/tos_data_processor/__init__.py
@@ -67,12 +67,13 @@ alc = on_alconna(
 )
 
 try:
-    from nonebot.adapters.onebot.v11 import GROUP, MessageEvent
+    from nonebot.adapters.onebot.v11 import GROUP as OB11GROUP
     from nonebot.adapters.onebot.v11 import Bot as OB11Bot
+    from nonebot.adapters.onebot.v11 import MessageEvent as OB11MessageEvent
 
     @alc.assign('query')
-    async def _(bot: OB11Bot, event: MessageEvent, matcher: Matcher, target: At | Me):
-        if event.is_tome() and await GROUP(bot, event):
+    async def _(bot: OB11Bot, event: OB11MessageEvent, matcher: Matcher, target: At | Me):
+        if event.is_tome() and await OB11GROUP(bot, event):
             await matcher.finish('不能查询bot的信息')
         proc = Processor(
             event_id=id(event),
@@ -84,6 +85,24 @@ try:
         except RequestError as e:
             if '未找到此用户' in e.message:
                 matcher.skip()
+        except NeedCatchError as e:
+            await matcher.send(str(e))
+            raise HandleNotFinishedError from e
+except ImportError:
+    pass
+
+try:
+    from nonebot.adapters.kaiheila.event import MessageEvent as KookMessageEvent
+
+    @alc.assign('query')
+    async def _(event: KookMessageEvent, matcher: Matcher, target: At | Me):
+        proc = Processor(
+            event_id=id(event),
+            user=User(teaid=f'kook-{target.target}' if isinstance(target, At) else f'kook-{event.get_user_id()}'),
+            command_args=[],
+        )
+        try:
+            await matcher.finish(await proc.handle_query())
         except NeedCatchError as e:
             await matcher.send(str(e))
             raise HandleNotFinishedError from e
