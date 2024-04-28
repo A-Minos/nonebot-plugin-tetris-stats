553ccf3f562b54a291650ea2f82bf661341c8765
shoucandanghehe <wallfjjd@gmail.com>
2024-04-29 00:21:13 +0800

    ✨ 使用 playwright 渲染网页

---
+++ nonebot_plugin_tetris_stats/utils/screenshot.py
@@ -0,0 +1,10 @@
+from .browser import BrowserManager
+
+
+async def screenshot(url: str) -> bytes:
+    browser = await BrowserManager.get_browser()
+    async with (
+        await browser.new_page(no_viewport=True, viewport={'width': 0, 'height': 0}) as page,
+    ):
+        await page.goto(url)
+        return await page.screenshot(full_page=True, type='png')
