456d9d8b058c1df542b2d97a99499f2c3f960ae0
shoucandanghehe <wallfjjd@gmail.com>
2024-04-30 01:47:23 +0800

    ✨ 支持茶服多个api地址的故障转移

--- nonebot_plugin_tetris_stats/game_data_processor/tos_data_processor/constant.py
+++ nonebot_plugin_tetris_stats/game_data_processor/tos_data_processor/constant.py
@@ -1,4 +1,10 @@
 from typing import Literal
 
 GAME_TYPE: Literal['TOS'] = 'TOS'
-BASE_URL = 'https://teatube.cn:8888/'
+BASE_URL = {
+    'https://teatube.cn:8888/',
+    'http://cafuuchino1.studio26f.org:19970',
+    'http://cafuuchino2.studio26f.org:19970',
+    'http://cafuuchino3.studio26f.org:19970',
+    'http://cafuuchino4.studio26f.org:19970',
+}
