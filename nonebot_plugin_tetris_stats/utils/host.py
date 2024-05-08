b629270bf582dec4f1a86886e343d1dd75353bad
shoucandanghehe <wallfjjd@gmail.com>
2024-05-09 07:21:45 +0800

    🔥 删除 identicon 相关代码

--- nonebot_plugin_tetris_stats/utils/host.py
+++ nonebot_plugin_tetris_stats/utils/host.py
@@ -2,17 +2,14 @@ from hashlib import sha256
 from ipaddress import IPv4Address, IPv6Address
 from typing import ClassVar
 
-from aiofiles import open
-from fastapi import FastAPI, Query, Response, status
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi import FastAPI, status
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from nonebot import get_app, get_driver
-from nonebot.log import logger
 from nonebot_plugin_localstore import get_cache_dir  # type: ignore[import-untyped]
 from pydantic import IPvAnyAddress
 
 from ..templates import path
-from .avatar import generate_identicon
 
 app = get_app()
 
@@ -54,19 +51,6 @@ async def _(page_hash: str) -> HTMLResponse:
     if page_hash in HostPage.pages:
         return HTMLResponse(HostPage.pages[page_hash])
     return NOT_FOUND
-
-
-@app.get('/identicon')
-async def _(md5: str = Query(regex=r'^[a-fA-F0-9]{32}$')):
-    identicon_path = cache_dir / 'identicon' / f'{md5}.svg'
-    if identicon_path.exists() is False:
-        identicon_path.parent.mkdir(parents=True, exist_ok=True)
-        result = await generate_identicon(md5)
-        async with open(identicon_path, mode='xb') as file:
-            await file.write(result)
-        return Response(result, media_type='image/svg+xml')
-    logger.debug('Identicon Cache hit!')
-    return FileResponse(identicon_path, media_type='image/svg+xml')
 
 
 def get_self_netloc() -> str:
