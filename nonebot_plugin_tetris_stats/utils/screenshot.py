bfe931d3bf4e2121791b0d8faa2bc02b4fddcbe4
shoucandanghehe <wallfjjd@gmail.com>
2024-06-04 20:15:41 +0800

    ✨ 为截图添加自动重试

--- nonebot_plugin_tetris_stats/utils/screenshot.py
+++ nonebot_plugin_tetris_stats/utils/screenshot.py
@@ -1,11 +1,15 @@
+from playwright.async_api import TimeoutError
+
 from .browser import BrowserManager
+from .retry import retry
 
 
+@retry(exception_type=TimeoutError, reply='截图失败, 重试中')
 async def screenshot(url: str) -> bytes:
     browser = await BrowserManager.get_browser()
     async with (
         await browser.new_page(no_viewport=True, viewport={'width': 0, 'height': 0}) as page,
     ):
         await page.goto(url)
-        await page.wait_for_load_state('networkidle')
-        return await page.screenshot(full_page=True, type='png')
+        await page.wait_for_load_state('networkidle', timeout=5000)
+        return await page.screenshot(timeout=5000, full_page=True, type='png')
