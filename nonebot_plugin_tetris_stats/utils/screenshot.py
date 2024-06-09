314e1dede3e985befff7a75401e63c01d2f3678d
shoucandanghehe <wallfjjd@gmail.com>
2024-06-10 00:26:26 +0800

    🔥 去除截图等待状态时的超时

--- nonebot_plugin_tetris_stats/utils/screenshot.py
+++ nonebot_plugin_tetris_stats/utils/screenshot.py
@@ -11,5 +11,5 @@ async def screenshot(url: str) -> bytes:
         await browser.new_page(viewport={'width': 3000, 'height': 3000}) as page,
     ):
         await page.goto(url)
-        await page.wait_for_load_state('networkidle', timeout=5000)
+        await page.wait_for_load_state('networkidle')
         return await page.locator('id=content').screenshot(timeout=5000, type='png')
