4bb00cdeb7be99895cc2a64312afc35d5bb5e089
shoucandanghehe <wallfjjd@gmail.com>
2024-08-06 20:11:50 +0800

    👽️ 移除茶服不可用地址

--- nonebot_plugin_tetris_stats/games/tos/constant.py
+++ nonebot_plugin_tetris_stats/games/tos/constant.py
@@ -6,9 +6,6 @@ GAME_TYPE: Literal['TOS'] = 'TOS'
 BASE_URL = {
     'https://teatube.cn:8888/',
     'http://cafuuchino1.studio26f.org:19970',
-    'http://cafuuchino2.studio26f.org:19970',
-    'http://cafuuchino3.studio26f.org:19970',
-    'http://cafuuchino4.studio26f.org:19970',
 }
 
 USER_NAME = compile(
