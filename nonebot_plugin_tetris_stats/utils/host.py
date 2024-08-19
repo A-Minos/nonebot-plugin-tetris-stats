fc6db40510eb10ffe00115b20292cdc494cbe045
shoucandanghehe <wallfjjd@gmail.com>
2024-08-20 06:44:18 +0800

    ♻️ 重构 resource 接口

--- nonebot_plugin_tetris_stats/utils/host.py
+++ nonebot_plugin_tetris_stats/utils/host.py
@@ -1,16 +1,19 @@
 from hashlib import sha256
 from ipaddress import IPv4Address, IPv6Address
+from pathlib import Path as FilePath
 from typing import TYPE_CHECKING, ClassVar, Literal
 
-from fastapi import FastAPI, Path, status
+from aiofiles import open
+from fastapi import BackgroundTasks, FastAPI, Path, status
 from fastapi.responses import FileResponse, HTMLResponse, Response
 from fastapi.staticfiles import StaticFiles
 from nonebot import get_app, get_driver
 from nonebot.log import logger
+from yarl import URL
 
 from ..config.config import CACHE_PATH
+from ..games.tetrio.api.cache import request
 from .image import img_to_png
-from .request import Request
 from .templates import TEMPLATES_DIR
 
 if TYPE_CHECKING:
@@ -22,6 +25,7 @@ driver = get_driver()
 
 global_config = driver.config
 
+BASE_URL = URL('https://tetr.io/user-content/')
 
 if not isinstance(app, FastAPI):
     msg = '本插件需要 FastAPI 驱动器才能运行'
@@ -63,18 +67,27 @@ def _(page_hash: str) -> HTMLResponse:
 
 @app.get('/host/resource/tetrio/{resource_type}/{user_id}', status_code=status.HTTP_200_OK)
 async def _(
-    resource_type: Literal['avatars', 'banners'], revision: int, user_id: str = Path(regex=r'^[a-f0-9]{24}$')
+    resource_type: Literal['avatars', 'banners'],
+    revision: int,
+    background_tasks: BackgroundTasks,
+    user_id: str = Path(regex=r'^[a-f0-9]{24}$'),
 ) -> Response:
     if not (path := CACHE_PATH / 'tetrio' / resource_type / f'{user_id}_{revision}.png').exists():
-        path.parent.mkdir(parents=True, exist_ok=True)
-        path.write_bytes(
-            img_to_png(
-                await Request.request(
-                    f'https://tetr.io/user-content/{resource_type}/{user_id}.jpg?rv={revision}', is_json=False
-                )
+        image = img_to_png(
+            await request.request(
+                BASE_URL / resource_type / f'{user_id}.jpg' % {'rv': revision},
+                is_json=False,
             )
         )
+        background_tasks.add_task(write_cache, path=path, data=image)
+        return Response(content=image, media_type='image/png')
     return FileResponse(path)
+
+
+async def write_cache(path: FilePath, data: bytes) -> None:
+    path.parent.mkdir(parents=True, exist_ok=True)
+    async with open(path, 'wb') as file:
+        await file.write(data)
 
 
 def get_self_netloc() -> str:
