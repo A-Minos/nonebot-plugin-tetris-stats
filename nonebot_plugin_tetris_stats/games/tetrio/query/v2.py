8c011859ddd2527f553d1d2171d927137e6c60cf
shoucandanghehe <wallfjjd@gmail.com>
2024-10-26 17:41:54 +0800

    ✨ TETR.IO 适配 v1 模板

--- nonebot_plugin_tetris_stats/games/tetrio/query/v2.py
+++ nonebot_plugin_tetris_stats/games/tetrio/query/v2.py
@@ -1,160 +1,37 @@
 from asyncio import gather
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 from hashlib import md5
-from typing import TypeVar
 
-from arclet.alconna import Arg, ArgFlag
-from nonebot import get_driver
-from nonebot.adapters import Event
-from nonebot.matcher import Matcher
-from nonebot_plugin_alconna import Args, At, Option, Subcommand
-from nonebot_plugin_alconna.uniseg import UniMessage
-from nonebot_plugin_orm import get_session
-from nonebot_plugin_session import EventSession
-from nonebot_plugin_session_orm import get_session_persist_id  # type: ignore[import-untyped]
-from nonebot_plugin_user import User as NBUser
-from nonebot_plugin_user import get_user
-from sqlalchemy import select
 from yarl import URL
 
-from ...db import query_bind_info, trigger
-from ...utils.host import HostPage, get_self_netloc
-from ...utils.metrics import get_metrics
-from ...utils.render import render
-from ...utils.render.schemas.base import Avatar
-from ...utils.render.schemas.tetrio.user.info_v2 import (
+from ....utils.host import HostPage, get_self_netloc
+from ....utils.metrics import get_metrics
+from ....utils.render import render
+from ....utils.render.schemas.base import Avatar
+from ....utils.render.schemas.tetrio.user.info_v2 import (
     Badge,
     Blitz,
+    Info,
     Sprint,
     Statistic,
     TetraLeague,
     TetraLeagueStatistic,
+    User,
     Zen,
 )
-from ...utils.render.schemas.tetrio.user.info_v2 import Info as V2TemplateInfo
-from ...utils.render.schemas.tetrio.user.info_v2 import User as V2TemplateUser
-from ...utils.screenshot import screenshot
-from ...utils.typing import Me
-from .. import add_block_handlers, alc
-from ..constant import CANT_VERIFY_MESSAGE
-from . import command, get_player
-from .api import Player
-from .api.schemas.summaries.league import NeverPlayedData, NeverRatedData
-from .constant import GAME_TYPE
-from .models import TETRIOUserConfig
-from .typing import Template
-
-UTC = timezone.utc
-
-driver = get_driver()
-
-command.add(
-    Subcommand(
-        'query',
-        Args(
-            Arg(
-                'target',
-                At | Me,
-                notice='@想要查询的人 / 自己',
-                flags=[ArgFlag.HIDDEN, ArgFlag.OPTIONAL],
-            ),
-            Arg(
-                'account',
-                get_player,
-                notice='TETR.IO 用户名 / ID',
-                flags=[ArgFlag.HIDDEN, ArgFlag.OPTIONAL],
-            ),
-        ),
-        Option(
-            '--template',
-            Arg('template', Template),
-            alias=['-T'],
-            help_text='要使用的查询模板',
-        ),
-        help_text='查询 TETR.IO 游戏信息',
-    ),
-)
-
-alc.shortcut(
-    '(?i:io)(?i:查询|查|query|stats)',
-    command='tstats TETR.IO query',
-    humanized='io查',
-)
-alc.shortcut(
-    'fkosk',
-    command='tstats TETR.IO query',
-    arguments=['我'],
-    fuzzy=False,
-    humanized='An Easter egg!',
-)
-
-add_block_handlers(alc.assign('TETRIO.query'))
-
-
-@alc.assign('TETRIO.query')
-async def _(  # noqa: PLR0913
-    user: NBUser,
-    event: Event,
-    matcher: Matcher,
-    target: At | Me,
-    event_session: EventSession,
-    template: Template | None = None,
-):
-    async with trigger(
-        session_persist_id=await get_session_persist_id(event_session),
-        game_platform=GAME_TYPE,
-        command_type='query',
-        command_args=[f'--template {template}'] if template is not None else [],
-    ):
-        async with get_session() as session:
-            bind = await query_bind_info(
-                session=session,
-                user=await get_user(
-                    event_session.platform, target.target if isinstance(target, At) else event.get_user_id()
-                ),
-                game_platform=GAME_TYPE,
-            )
-            if template is None:
-                template = await session.scalar(
-                    select(TETRIOUserConfig.query_template).where(TETRIOUserConfig.id == user.id)
-                )
-        if bind is None:
-            await matcher.finish('未查询到绑定信息')
-        message = UniMessage(CANT_VERIFY_MESSAGE)
-        player = Player(user_id=bind.game_account, trust=True)
-        await (message + UniMessage.image(raw=await make_query_image_v2(player))).finish()
-
-
-@alc.assign('TETRIO.query')
-async def _(user: NBUser, account: Player, event_session: EventSession, template: Template | None = None):
-    async with trigger(
-        session_persist_id=await get_session_persist_id(event_session),
-        game_platform=GAME_TYPE,
-        command_type='query',
-        command_args=[f'--template {template}'] if template is not None else [],
-    ):
-        async with get_session() as session:
-            if template is None:
-                template = await session.scalar(
-                    select(TETRIOUserConfig.query_template).where(TETRIOUserConfig.id == user.id)
-                )
-        await (UniMessage.image(raw=await make_query_image_v2(account))).finish()
-
-
-N = TypeVar('N', int, float)
-
-
-def handling_special_value(value: N) -> N | None:
-    return value if value != -1 else None
+from ....utils.screenshot import screenshot
+from ..api import Player
+from ..api.schemas.summaries.league import NeverPlayedData, NeverRatedData
+from .tools import flow_to_history, handling_special_value
 
 
 async def make_query_image_v2(player: Player) -> bytes:
     (
         (user, user_info, league, sprint, blitz, zen),
-        (avatar_revision, banner_revision),
+        (avatar_revision, banner_revision, leagueflow),
     ) = await gather(
         gather(player.user, player.get_info(), player.league, player.sprint, player.blitz, player.zen),
-        gather(player.avatar_revision, player.banner_revision),
+        gather(player.avatar_revision, player.banner_revision, player.get_leagueflow()),
     )
     if sprint.data.record is not None:
         duration = timedelta(milliseconds=sprint.data.record.results.stats.finaltime).total_seconds()
@@ -176,8 +53,8 @@ async def make_query_image_v2(player: Player) -> bytes:
     async with HostPage(
         await render(
             'v2/tetrio/user/info',
-            V2TemplateInfo(
-                user=V2TemplateUser(
+            Info(
+                user=User(
                     id=user.ID,
                     name=user.name.upper(),
                     bio=user_info.data.bio,
@@ -227,7 +104,7 @@ async def make_query_image_v2(player: Player) -> bytes:
                     adpl=metrics.adpl,
                     statistic=TetraLeagueStatistic(total=league.data.gamesplayed, wins=league.data.gameswon),
                     decaying=league.data.decaying,
-                    history=None,
+                    history=flow_to_history(leagueflow),
                 )
                 if not isinstance(league.data, NeverPlayedData)
                 else None,
