1cb5be063051e2ef8bca71281999ed6499954dd7
shoucandanghehe <wallfjjd@gmail.com>
2026-03-26 11:23:22 +0800

    ✨ 同步模板的 schema

--- nonebot_plugin_tetris_stats/utils/render/schemas/v2/tetrio/tetra_league/detail.py
+++ nonebot_plugin_tetris_stats/utils/render/schemas/v2/tetrio/tetra_league/detail.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from pydantic import BaseModel
 from typing_extensions import override
 
-from .....typedefs import Number
-from ...base import Base
+from ......typedefs import Number
+from ....base import Base
 
 
 class StatisticalData(BaseModel):
@@ -31,7 +31,7 @@ class Data(Base):
     @property
     @override
     def path(self) -> str:
-        return 'v2/tetrio/tetra-league'
+        return 'v2/tetrio/tetra-league/detail'
 
     replay_id: str
     games: list[Game]
